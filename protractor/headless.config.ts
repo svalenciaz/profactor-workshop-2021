25590dc06bea7c1b438d129ac9774bbfd1b22daf
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-10-27 02:02:30 -0500

    commit before debugging

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -5,6 +5,12 @@ export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/google.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu'],
+    },
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
