b3d1719258fca9178f9a588891cc4db8172d757a
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-10-28 01:42:13 -0500

    report-awesome configured

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,9 +1,20 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
 
-export const reporter = () => {
+const { AwesomeReport } = require('jasmine-awesome-report');
+
+const awesomeReportConfig = {
+  fullPath: 'reports',
+  fileName: 'awesome',
+  merge: true,
+};
+
+const reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: StacktraceOption.PRETTY,
     },
   }));
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(awesomeReportConfig));
 };
+
+export { reporter };
